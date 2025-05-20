8582f6efe1fa648641cfd50c2ec1c61764bcb501
Aleksandrs Gusevs <aleksandrs.gusevs@doublehelix.dev>
2025-05-20 11:56:51 +0300

    Change order of services

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -3,14 +3,14 @@
 export default function Services() {
   const services = [
     {
-      title: 'Software Development',
-      description: 'Custom IT solutions tailored to your business needs, built with modern technologies and best practices. Secure, scalable, and simple.',
+      title: 'Integrations & Process Automation',
+      description: 'Streamline your business operations through intelligent automation and system integration. Transform your workflows to prepare for the digital future. Get the most out of your resources.',
       icon: (
         <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
         </svg>
       ),
-    },
+    },   
     {
       title: 'IT Consultancy',
       description: 'Strategic technology consulting to help you make informed decisions. We break down problems and map out business-smart, tech-driven paths to results.',
@@ -19,13 +19,13 @@ export default function Services() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
         </svg>
       ),
-    },
+    }, 
     {
-      title: 'Process Automation',
-      description: 'Streamline your business operations through intelligent automation and system integration. Transform your workflows to prepare for the digital future. Get the most out of your resources.',
+      title: 'Software Development',
+      description: 'Custom IT solutions tailored to your business needs, built with modern technologies and best practices. Secure, scalable, and simple.',
       icon: (
         <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
         </svg>
       ),
     },
